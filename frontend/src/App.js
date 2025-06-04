93447d99d4e90c8da7303238f4a3d809eb1b0be0
NishatTasnimPreownti <preownti@gmail.com>
2025-06-05 02:23:29 +0600

    Add landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { LoginSignup } from './Components/LoginSignup/LoginSignup';
-
+import LandingPage from './Components/LandingPage/LandingPage'
 function App() {
   return (
     <div>
+      <LandingPage />
       <LoginSignup />
     </div>
   );
---
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      {/* Header */}
+      <header className="header">
+        <div className="logo"> RESUMIX </div>
+        <nav>
+          <ul>
+            <li>Resume</li>
+            <li>Resources</li>
+            <li>Premium Services</li>
+            
+          </ul>
+        </nav>
+        <div className="auth-buttons">
+          <button className="login-btn">Login</button>
+          <button className="register-btn">Signup</button>
+        </div>
+      </header>
+
+      {/* Hero Section */}
+      <section className="hero">
+        <h1>Create a Job-Ready Resume in Few Minutes.</h1>
+        <p className="subtext">Create your resume with our free builder and professional templates.</p>
+        <button className="primary-btn">Build my Resume</button>
+      </section>
+
+
+      {/* Live Preview */}
+      <section className="preview">
+        <h1>Quick, Easy And Flexible Editing With Live Preview</h1>
+        <p className="subtext">
+        Choose font types, sizes, and spacing. You can bold, underline, and italicize your text.
+        </p>
+      </section>
+
+
+      {/* Steps Section */}
+      <section className="steps">
+        
+        <div className="step-boxes">
+          <div className="step">1. <p>Choose a Free Resume Template</p></div>
+          <div className="step">2. <p>Fill in Your Details</p></div>
+          <div className="step">3. <p>Customize Your Resume</p></div>
+          <div className="step">4. <p>Share as PDF or Web</p></div>
+        </div>
+      </section>
+
+
+      {/* Templates Section */}
+      <section className="templates">
+        <h4>OPTIMIZED DESIGNS</h4>
+        <h2>Make Your Resume With Proven And Professional Templates</h2>
+        <p>Use one of our field-tested resume templates, designed by experts and typographers.</p>
+        <div className="features">
+          <span> ATS-Friendly</span>
+          <span> Professional</span>
+          <span> Modern</span>
+          <span> Creative</span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
