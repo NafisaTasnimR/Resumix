9b60924c3d7376ae5cf3d98a9a15d25eff2d1078
nafisa <nafisatasnim8855@gmail.com>
2025-06-04 11:57:21 +0600

    Create new Sidebar.jsx file
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

---
+++ frontend/src/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Sidebar = () => (
+    <div className="sidebar">
+        <button>
+            <img src="" alt="Template" width="30" height="30"/>
+            <span>Template</span>
+        </button>
+        <button>
+            <img src="" alt="Font" width="30" height="30"/>
+            <span>Font</span>
+        </button>
+        <button>
+            <img src="" alt="Color Palette" width="30" height="30"/>
+            <span>Color Palette</span>
+        </button>
+        <button>
+            <img src="" alt="ATS Checker" width="30" height="30"/>
+            <span>ATS Checker</span>
+        </button>
+    </div>
+);
+
+export default Sidebar;
