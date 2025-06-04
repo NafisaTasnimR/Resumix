91099b541e6e9e6d320eb7e83ca0777388df25ba
nafisa <nafisatasnim8855@gmail.com>
2025-06-04 11:58:38 +0600

    Add Sidebar component
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TopBar from './TopBar';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <TopBar></TopBar>
+      <Sidebar></Sidebar>
     </div>
   );
 }
