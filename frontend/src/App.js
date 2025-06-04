9e2790c3e53232edad550bdfa784df5ea92dd74c
nafisa <nafisatasnim8855@gmail.com>
2025-06-04 13:13:23 +0600

    Modify App.css file to implement css styling on ResumeEditor component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TopBar from './TopBar';
 import Sidebar from './Sidebar';
+import ResumeEditor from './ResumeEditor';
 
 function App() {
   return (
     <div className="App">
       <TopBar></TopBar>
       <Sidebar></Sidebar>
+      <ResumeEditor></ResumeEditor>
     </div>
   );
 }
