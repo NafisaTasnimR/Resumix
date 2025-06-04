64483eb9f8208ce5a036e29b862d7c6e25625b25
nafisa <nafisatasnim8855@gmail.com>
2025-06-04 13:19:45 +0600

    Add new Preview.jsx file to create live preview section

---
+++ frontend/src/Preview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Preview = () => (
+  <div className="preview">
+    <div className="preview-box">Resume Live Preview</div>
+  </div>
+);
+
+export default Preview;
