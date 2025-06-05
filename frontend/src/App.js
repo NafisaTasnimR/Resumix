a9f1568fb6b6f3a90e266e6c7574309ec946f4b3
nafisa <nafisatasnim8855@gmail.com>
2025-06-05 16:40:59 +0600

    Add layout and styling for resume editor with A4 preview layout   - Added .resume-editor as a flex container with sidebar and main sections   - Created fixed A4-style .preview-box inside .live-preview   - Ensured no vertical scroll and proper spacing with top-bar offset   - Improved responsiveness and visual alignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import TopBar from './TopBar';
 import Sidebar from './Sidebar';
 import ResumeEditor from './ResumeEditor';
-import Preview from './Preview';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <TopBar></TopBar>
       <Sidebar></Sidebar>
       <ResumeEditor></ResumeEditor>
-      <Preview></Preview>
     </div>
   );
 }
