a3d1d2ebf0d2e9fc20c2976733a402c07f57a3b2
nafisa <nafisatasnim8855@gmail.com>
2025-06-10 18:02:53 +0600

    Move the necessary css styling from App.css to ResumeEditorPage.css
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import TopBar from './TopBar';
-import Sidebar from './Sidebar';
-import ResumeEditor from './ResumeEditor';
+import ResumeEditorPage from './ResumeEditorPage';
 
 function App() {
   return (
     <div className="App">
-      <TopBar></TopBar>
-      <Sidebar></Sidebar>
-      <ResumeEditor></ResumeEditor>
+      <ResumeEditorPage></ResumeEditorPage>
     </div>
   );
 }
