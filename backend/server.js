87c67a4153a6448430ce9d8773809eb3e8064fc6
nafisa <nafisatasnim8855@gmail.com>
2025-06-21 17:29:38 +0600

    Add server.js to initialize Express server and set up auth routes
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
