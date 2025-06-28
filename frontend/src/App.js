a7ec8c938b8a84a63c1227b60add411d4bfc124d
NishatTasnimPreownti <preownti@gmail.com>
2025-06-29 00:36:21 +0600

    Create routing component to handle the routes in landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import SettingsPage from './Components/Settings/SettingsPage';
+import PostLoginHeader from './Components/PostLoginHeader/PostLoginHeader';
+import RoutingComponent from './Components/RoutingComponent';
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
         <Route path="/login" element={<LoginSignup mode="login" />} />
         <Route path="/signup" element={<LoginSignup mode="signup" />} />
         <Route path="/settings" element={<SettingsPage />} />
-
+        <Route path="/postlogin/" element={<PostLoginHeader />} />
       </Routes>
+      <RoutingComponent />
+     
     </Router>
   );
 }
