bc649b54c08f8bf9f8f3bd1560bda5e277d5f127
Mriitika150 <mrittikajahan@iut-dhaka.edu>
2025-07-12 22:19:13 +0600

    Update some design changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,6 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import SettingsPage from './Components/Settings/SettingsPage';
 import PostLoginHeader from './Components/PostLoginHeader/PostLoginHeader';
-import SubscriptionPage from './Components/SubscriptionPage/SubscriptionPage';
-import PaymentInfo from './Components/PaymentInfo/PaymentInfo';
-import ProfileForm from './Components/ProfileForm/ProfileForm';
-import FinishPage from './Components/FinishPage/FinishPage';
 function App() {
   return (
     <Router>
@@ -16,10 +12,6 @@ function App() {
         <Route path="/login" element={<LoginSignup mode="login" />} />
         <Route path="/signup" element={<LoginSignup mode="signup" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/subscription" element={<SubscriptionPage />} />
-        <Route path="/payment" element={<PaymentInfo />} />
-        <Route path="/profile" element={<ProfileForm />} />
-        <Route path="/final" element={<FinishPage />} />
       </Routes>
     </Router>
   );
