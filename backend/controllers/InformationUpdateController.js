a1ded8a01fc731a611d4f04f586b85fb41f49cc3
nafisa <nafisatasnim8855@gmail.com>
2025-07-22 20:28:25 +0600

    Modify InformationUpdateController.js file to 		add a new function for retrieving the data from database
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- backend/controllers/InformationUpdateController.js
+++ backend/controllers/InformationUpdateController.js
@@ -24,6 +24,21 @@ const updateInformation = async (req, res) => {
     }
 };
 
+const getUserInformation = async (req, res) => {
+    try {   
+        const userId = req.user.userId;
+        const userData = await user.findById(userId, 'username defaultResumeData');
+        if (!userData) {    
+            return res.status(404).json({ message: 'User not found' });
+        }               
+        res.status(200).json(userData);
+    } catch (error) {   
+        console.error("Get User Information Error:", error);
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+};
+
 module.exports = {
-    updateInformation
+    updateInformation,
+    getUserInformation
 };           
