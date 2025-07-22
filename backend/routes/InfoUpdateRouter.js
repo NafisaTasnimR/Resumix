a5b0174382b136b0994e0e4e195fc023b4f09da6
nafisa <nafisatasnim8855@gmail.com>
2025-07-22 20:23:47 +0600

    Modify InfoUpdateRouter file to add the route of get data from database
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- backend/routes/InfoUpdateRouter.js
+++ backend/routes/InfoUpdateRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { updateInformation } = require('../controllers/InformationUpdateController');
-const { verifyToken, validateInformationUpdate } = require('../middlewares/ResumeDataValidation');
+const { validateInformationUpdate } = require('../middlewares/ResumeDataValidation');
+const { getUserInformation } = require('../controllers/InformationUpdateController');
+const { verifyToken } = require('../middlewares/TokenVerification');
 
 router.patch('/update',verifyToken,validateInformationUpdate, updateInformation);
+router.get('/userInformation',verifyToken, getUserInformation);
 
 module.exports = router;
