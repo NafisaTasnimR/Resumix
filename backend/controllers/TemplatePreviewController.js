0b13414c8e7bf16c531fe590bb41a556bbaa4b05
nafisa <nafisatasnim8855@gmail.com>
2025-07-27 15:07:44 +0600

    Add new getTemplatePartsById to send template html file's elements part by part
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- backend/controllers/TemplatePreviewController.js
+++ backend/controllers/TemplatePreviewController.js
@@ -2,6 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 // Controller to get a single template by ID
+exports.getTemplatePartsById = (req, res) => {
+  fs.readFile(`./templates/${req.params.id}.html`, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Template not found");
+    //res.send(data);
+    const styleMatch = data.match(/<style[^>]*>([\s\S]*?)<\/style>/);
+    const bodyMatch = data.match(/<body[^>]*>([\s\S]*?)<\/body>/);
+
+    const templateCss = styleMatch ? styleMatch[1] : "";
+    const rawTemplate = bodyMatch ? bodyMatch[1] : "";
+
+    res.json({
+      rawTemplate,
+      templateCss,
+      templateName: `Template ${req.params.id}`
+    });
+
+  });
+};
+
 exports.getTemplateById = (req, res) => {
   fs.readFile(`./templates/${req.params.id}.html`, "utf8", (err, data) => {
     if (err) return res.status(500).send("Template not found");
